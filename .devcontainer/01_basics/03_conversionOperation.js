7d657840e7ad85299b96fa9d2084b057d179c491
ishika-164 <choudharyishika164@gmail.com>
2025-05-10 16:17:53 +0000

    conversion operations done

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score=true
+
+//console.log(typeof score); //pehla tarika to see datatype of variable
+//console.log(typeof(score));  ///dusra tarika to see datatype of variable
+
+let valueinnum=Number(score) //coverts the datatype of var score -> number
+//console.log(valueinnum);
+//console.log(typeof(valueinnum));
+
+//"33" -> 33
+//"Ishika" -> NaN (not a number) but its datatype is still a number
+//true ->1 ;false ->0
+
+let isLoggedIn="Ishika"
+
+let booleaniLoggedIn=Boolean(isLoggedIn)
+//console.log(booleaniLoggedIn);
+//console.log(typeof(booleaniLoggedIn));
+
+//"" -> false (empty string)
+//" Ishika" -> true
+// (any number) -> true ;0 -> false
+
+let number=33
+
+let stringNumber=String(number)
+console.log(stringNumber);
+console.log(typeof(stringNumber));

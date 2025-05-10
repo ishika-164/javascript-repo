918e3e2c3f51d4b8aa97cb38e737b99b817c059c
ishika-164 <choudharyishika164@gmail.com>
2025-05-10 09:28:26 +0000

    done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=12345
+let accountEmail="ishika@gmail.com"
+var accountPass="123456"
+accountCity="Indore"
+
+accountEmail="ishi@gmail.com"
+accountPass="121212"
+accountCity="Goa"
+
+console.table([accountId,accountEmail,accountPass,accountCity])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+console.log(3+
+    3   // code readibility should be the priority 
+)
+let name="Ishika" //string datatype
+let age=20 //number
+
+//numbre=2 ot the poer 53
+//bigInt
+//string =" "
+//boolean
+//null =standalone value
+//undefined
+//symbol =uniqueness
+//object
+
+let state;
+console.log(state) //hen no value assigned it is undefined
+
+let City=null
+console.log(typeof City) //null's datatype is object
+console.log(typeof state) //undefined's datatype is undefined

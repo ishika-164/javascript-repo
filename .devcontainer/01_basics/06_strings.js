24704e4349061c6ed3f6093af9fc368f246a9586
ishika-164 <choudharyishika164@gmail.com>
2025-05-12 14:27:57 +0000

    strings done

---
+++ .devcontainer/01_basics/06_strings.js
@@ -0,0 +1,28 @@
+let name="Ishika"
+let repocount=2
+
+//console.log(name+repocount+" Value"); // not a good ting to represent
+
+console.log(`My name is ${name} and my repo count is ${repocount}`); //good 
+
+const gamename=new String('Ishikaaaa')
+//console.log(gamename[1]);
+//onsole.log(gamename.__proto__);
+
+console.log(gamename.indexOf('a'));
+const substring=gamename.substring(1,3)
+console.log(substring)
+
+const slicestring=gamename.slice(-8,4)  //negative means from back
+console.log(slicestring)
+
+const string1="   Ishika   "
+console.log(string1);
+console.log(string1.trim());  // others like trim start and trim end are also there
+
+const url="https://Ishika/Ishika.com$hello"
+console.log(url.replace('$','_'));
+console.log(url.includes('_'));
+
+console.log(url.split('/'));
+console.log(url.toWellFormed());

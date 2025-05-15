f68f70ff874474c3dd8728574e0efb367aaed5bf
ishika-164 <choudharyishika164@gmail.com>
2025-05-15 13:25:47 +0000

    IIFE done

---
+++ .devcontainer/03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// IIFE(Imediately Invoked Function Expressions)
+// WHY USE IIFE ??
+// imediately call krne ke liye aur IMP : global pollution se koi effect na ho isliye (global var se ya declarations se)
+
+//syntax: (function)(function execution)
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+//Simple IIFE (arrow function)
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('Ishika')

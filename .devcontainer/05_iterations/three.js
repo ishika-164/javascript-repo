99ade1a4aee80c965ba1d40cbf93e8484fe12111
ishika-164 <choudharyishika164@gmail.com>
2025-05-19 10:01:56 +0000

    done

---
+++ .devcontainer/05_iterations/three.js
@@ -0,0 +1,43 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+// synatx:
+// for (const element of object) {
+     // here object refers to the thing on which operations are going to be performed
+// }
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) { // through [key,value] we are destructuring the map
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }

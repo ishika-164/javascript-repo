181259a7129e4d1fcd99edad20c3b643cf073ab1
ishika-164 <choudharyishika164@gmail.com>
2025-05-19 11:08:12 +0000

    reduce done

---
+++ .devcontainer/05_iterations/nine.js
@@ -0,0 +1,37 @@
+const myNums = [1, 2, 3]
+// reduce i often used in shopping cart total calculation ,etc.
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)// 0 is initial value and acc+curr is going to go in acc
+
+//console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(`Total price of the cart: ${priceToPay} Rs`);
+
+//console.log(priceToPay);
--- .devcontainer/05_iterations/seven.js
+++ .devcontainer/05_iterations/seven.js
@@ -2,9 +2,10 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
+//chaining : ek method ke baad dusra method
 const newNums = myNumers
                 .map((num) => num * 10 )
-                .map( (num) => num + 1)
+                .map( (num) => num + 1) // yaha par jo upar method lagne ke baad array aaya hoga vo yaha pass hoga
                 .filter( (num) => num >= 40)
 
 console.log(newNums);
